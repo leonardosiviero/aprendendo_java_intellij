60d14d1ae612a9418e360f5fcb4cf32955327096
leonardosiviero <leosiviero@gmail.com>
2022-02-27 15:19:18 -0300

    first commit

---
+++ src/br/com/dio/primeiroProgramaJava.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+/*para rodar o programa java primeira vez: ctrl + shft + F10, depois é só shft + F10 */
+
+/*    Comentar bloco ctrl + shft + /       */
+/* clicar em cima do primeiroProgramaJava e ctrl + F6 para mudar o nome do programa */
+/* Ctrl + Alt + L organiza o  código */
+/* Ctrl + Y duplica linha // Ctrl + Y deleta linha */
+
+import br.com.dio.model.gato;
+
+public class primeiroProgramaJava {
+    public static void main(String[] args) {
+
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+/*        int a = 5;
+        int b = 3;
+        System.out.println("Hello World! Soma de a + b é " + (a+b));
+*/
+
+    }
+}
