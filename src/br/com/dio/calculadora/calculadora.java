874a7df1bba463284de0b90170d968ad5095a50c
leonardosiviero <leosiviero@gmail.com>
2022-02-27 16:11:02 -0300

    criando class livro

---
+++ src/br/com/dio/calculadora/calculadora.java
@@ -0,0 +1,45 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o valor de a: ");
+        a = scan.nextInt();
+        System.out.println("Digite o valor de b: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a,b);
+        int multiplicar = multiplicar(a,b);
+        int dividir = dividir(a,b);
+
+        System.out.println("Soma: " + somar);
+        System.out.println("Subtração : " + subtrair);
+        System.out.println("Multiplicação: " + multiplicar);
+        System.out.println("Divisão: " + dividir);
+
+    }
+
+    public static int somar(int a, int b){
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b){
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b){
+        return a * b;
+    }
+
+    public static int dividir(int a, int b){
+        return a / b;
+    }
+
+}
+
